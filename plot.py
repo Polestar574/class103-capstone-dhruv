d6c8f8d74687f697efe2ea2aef133573d449a58a
Polestar574 <73738038+Polestar574@users.noreply.github.com>
2021-08-23 17:54:27 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+import plotly.express as px
+
+#reading data from csv files
+df = pd.read_csv("line_chart.csv") 
+fig = px.line(df, x="Year", y="Per Capita Income",color="Country",title="Per Capita Income")
+fig.show()
